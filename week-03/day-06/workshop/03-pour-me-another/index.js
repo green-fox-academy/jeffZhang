d8f70069fe99ebd08f80b46d93af93d159af9ef7
Zhen <zhangzhengbaiw@gmail.com>
2019-10-13 15:41:08 +0800

    7: W3/D6 -- addition of 03-pour-me-another

---
+++ week-03/day-06/workshop/03-pour-me-another/index.js
@@ -0,0 +1,23 @@
+const URL = './data.json'
+
+const d = document
+const header = d.querySelector('h1')
+const button = d.querySelector('button')
+
+const fetchNames = url => {
+  return fetch(url).then(response => response.json())
+}
+
+const pourMeAnothr = async url => {
+  let { names: nameArray } = await fetchNames(url)
+  button.disabled = false //enable this button when the data is loaded
+  button.addEventListener('click', () => {
+    header.textContent = showNextName(nameArray).next().value
+  })
+}
+
+const showNextName = function*(nameArray) {
+  nameArray.length ? yield nameArray.shift() : yield 'no names any more'
+}
+
+pourMeAnothr(URL)
