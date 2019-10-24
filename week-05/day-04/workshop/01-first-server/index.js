aca64a39233fbb4af5c02cc20470cad9f9faf81c
Zhen <zhangzhengbaiw@gmail.com>
2019-10-24 12:43:41 +0800

    2: W5/D4 --addition of workshop 02-hello-world

--- week-05/day-04/workshop/01-first-server/index.js
+++ week-05/day-04/workshop/01-first-server/index.js
@@ -10,3 +10,5 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('app is running on 3000')
 })
+
+/*if the index.js is in a folder, and at the same time, package.json & node_modules in the upper level, it's required to cd to the folder and fire node in this folder. In this case, node(mon) index.js should be fired in folder 01-first-server, and the static path will be correct.*/
